54347b9dae404f8beff7fbca91d82db64989c869
Shubham <dev.shubh.am.dshah@gmail.com>
2020-06-02 18:05:13 +0530

    removed db file and merged it with signup file

--- Signup.py
+++ Signup.py
@@ -1,5 +1,6 @@
 from getpass import getpass
 from time import sleep
+import mysql.connector
 
 def greet():
     print ("Welcome to Fintra, tailored by The GINISSI Inc.")
@@ -32,9 +33,24 @@ def signup():
     pwd = getpass("Enter your password: ")
     pwd2 = getpass("Confirm password: ")
 
+def db_creation():
+    db_user = input("Enter MYSQL username: ")
+    db_password = getpass("Enter MYSQL password: ")
+    db_login = mysql.connector.connect(user=db_user, password=db_password, host='127.0.0.1')
+    db_cursor = db_login.cursor()
+    db_cursor.execute("show databases")
+    db_list = db_cursor.fetchall()
+    if (('fintra',)) in db_list:
+        pass
+    else:
+        db_cursor.execute("create database fintra")
+    db_cmd = ('create table', uname, '(RENT INTEGER(20), FOOD INTEGER(20), CLOTHING INTEGER(20), TRAVEL INTEGER(20), ENTERTAINMENT INTEGER(20), Miscellaneous INTEGER(20))')
+    db_cursor.execute(db_cmd)
+
 def run():
     while True:
         signup()
         authenticate()
+        db_creation()
         if su == True:
             break
